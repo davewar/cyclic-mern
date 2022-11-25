187a99d2e2c3ba9dce7b9358ce1a0625eb331a4e
dave <davewarwicker1@gmail.com>
2022-11-25 10:35:57 +0000

    >

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
 		<div className='container'>
 			<div className='row'>
 				<div className='col-xs-12'>
-					<h1 className='text-center mt-5 '>Welcome to my fullstack website</h1>
+					<h1 className='text-center mt-5 '>Welcome to My fullstack website</h1>
 
 					<h5 className='mt-5'>FrontEnd</h5>
 
